eaba74bdfab7427d98b8437dfd973ea5a6e6ac2d
YH1d3k12 <djalmahy@hotmail.com>
2023-10-11 19:50:11 -0300

    Feat: created initial structure

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import RootNavigation from './routes/rootNavigation.jsx';
+
+
+export default function App() {
+    return (
+        <RootNavigation />
+    );
+};
---
+++ src/routes/rootNavigation.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Body from "../screens/body.jsx";
+
+/**
+* Represents the root navigation setup for this application.
+* It utilizes React Router for handling different routes.
+*
+* @returns a specified screen based on the URL path.
+*/		
+export default function RootNavigation() {
+    return (
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
+			<Routes>
+                {/* The Body component serves as the top-level container for all other screens.*/}
+				<Route path="/" element={<Body  />}>
+					{/* 
+                      	The following components are child screens rendered within the Outlet of the Body component.
+                      	These components represent specific views or sections of the application.
+                    */}
+				</Route>
+			</Routes>
+	  </BrowserRouter>
+	);
+};
